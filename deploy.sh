f7105b6a93ec80492d6a30e06949ca41339b6d21
Emanuele Bin <bin.emanuele@hotmail.it>
2024-08-17 16:43:20 +0200

    Prepare for production

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SERVER_USER="bin-dev"
+SERVER_HOST="192.168.1.8"
+SERVER_DIR="/home/bin-dev/web/quiz.bin-dev.site/public_html/public"
+LOCAL_BUILD_DIR="public"  # The directory containing the built files
+
+npm install
+npm run build
+
+ssh $SERVER_USER@$SERVER_HOST "rm -rf $SERVER_DIR/*"
+
+scp -r $LOCAL_BUILD_DIR/* $SERVER_USER@$SERVER_HOST:$SERVER_DIR
+
+ssh $SERVER_USER@$SERVER_HOST << EOF
+  cd $SERVER_DIR
+  git pull
+  php artisan deploy
+EOF
+
+echo "Deployment completed!"
