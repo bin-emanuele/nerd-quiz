8b79fd7bde424f78d3b7ea9cd8092922da81c7ef
Emanuele Bin <bin.emanuele@hotmail.it>
2024-08-18 14:09:43 +0200

    Changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 SERVER_USER="bin-dev"
 SERVER_HOST="192.168.1.8"
-SERVER_DIR="/home/bin-dev/web/quiz.bin-dev.site/public_html/public"
-LOCAL_BUILD_DIR="public"  # The directory containing the built files
+SERVER_DIR="/home/bin-dev/web/quiz.bin-dev.site/public_html"
+LOCAL_PUB_DIR="public" # The directory containing the public files
 
 npm install
 npm run build
 
-ssh $SERVER_USER@$SERVER_HOST "rm -rf $SERVER_DIR/*"
+ssh $SERVER_USER@$SERVER_HOST "rm -rf $SERVER_DIR/public/*"
 
-scp .env $SERVER_USER@$SERVER_HOST:$SERVER_DIR
-scp -r $LOCAL_BUILD_DIR/* $SERVER_USER@$SERVER_HOST:$SERVER_DIR
+scp .env.production $SERVER_USER@$SERVER_HOST:$SERVER_DIR/.env
+scp -r $LOCAL_PUB_DIR/* $SERVER_USER@$SERVER_HOST:$SERVER_DIR/public
 
 ssh $SERVER_USER@$SERVER_HOST << EOF
   cd $SERVER_DIR
