3330054136b9e3683690363c7ffe5af8220c1f3b
Emanuele Bin <bin.emanuele@hotmail.it>
2024-08-20 11:57:59 +0200

    HOTFIX

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 defineProps<{
-    message?: string | string[];
+  message?: string | string[];
 }>();
 </script>
 
 <template>
-    <div v-show="message">
-        <p v-for="error in message" class="text-sm text-red-600 dark:text-red-400">
-            {{ error }}
-        </p>
-    </div>
+  <div v-show="message">
+    <p class="text-sm text-red-600 dark:text-red-400">
+      {{ message }}
+    </p>
+  </div>
 </template>
